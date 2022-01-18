1778f0344179ce0f0fa6be7bde545f892cbb44e8
Luis Medina Lozano <92281504+KoiGi01@users.noreply.github.com>
2022-01-17 18:28:35 -0600

    Update: added new button functionality

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 let currentMusic = 0;
 
-const music = document.getElementById("audio");
+var music = document.getElementById("audio");
+var music2 = document.getElementById("audio2");
+var currentSong = music;
 const disk = document.querySelector('.disk');
 const button = document.querySelector('.play-button');
+const skip = document.querySelector('.change');
+var order = music;
 
 button.addEventListener("click", playMusic);
+skip.addEventListener("click", changeSong);
 button.addEventListener("keydown", event => {
     if (event.code === 'Space') {
       playMusic();
@@ -13,11 +18,26 @@ button.addEventListener("keydown", event => {
 
 function playMusic() {
     //play-pause music --- cd animation
+    currentSong = order;
     button.classList.toggle('pause');
     disk.classList.toggle('play');
 
     if(button.className.includes('pause')){
-        music.play();
-    } else { music.pause();
+        currentSong.play();
+    } else { currentSong.pause();
     }
+}
+function changeSong() {
+
+  button.classList.toggle('pause');
+  disk.classList.toggle('play');
+  currentSong.pause();
+  
+  if(currentSong == music){
+      order = music2;
+      console.log(currentSong);
+  } else {
+      order = music;
+  }
+  
 }
